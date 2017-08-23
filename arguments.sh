095b0ae20dd6973bf7f2696903628f3200e8e9bb
Tim Haskins <tijohask@iupui.edu>
2017-08-23 17:21:05 -0400

    Added a script demonstrating how arguments can be passed and used in the script.

---
+++ arguments.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# The variable $# returns the number of arguments passed to the program (sort of)
+echo Number of Args: $#
+
+# The variable $@ returns a list of the values of the arguments
+echo ""
+echo "All the passed arguments, in order: "
+for f in $@
+do
+	echo $f
+done
+
+# The arguments themselves can be accessed by $[place of argument]
+echo ""
+echo "The first argument was: "
+if [ $# -ge 1 ]
+then
+    echo $1
+fi
+
+# The program will always have a $0 argument (the name of the program itself)
+echo ""
+echo "This program is: " $0
+
+pwd
