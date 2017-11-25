eb77dedc542190dbe164933b530c80ddb9c5ecb2
Tim Haskins <tijohask@iupui.edu>
2017-11-25 12:46:22 -0500

    Removed files that do not follow standard naming convention.

--- dateFile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#If the user inputs an argument to the script, then the program will 
-#   create a new file in the format [time]_[argument]
-if [ $# -ge 1 ]
-then
-	touch $(date +%T)_$1
-fi
